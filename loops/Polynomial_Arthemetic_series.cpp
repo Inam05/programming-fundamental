037e3c024c45bf4bc211db73445015c238089778
Inam05 <inamulhassan.tech@gmail.com>
2024-07-09 09:45:57 +0500

    How to find polynomial arthemetic series by loop in c++ ?

---
+++ loops/Polynomial_Arthemetic_series.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 100; i += 3)
+    {
+        int term;
+
+        if (i % 2 == 0)
+        {
+            term = -i;
+        }
+        else
+        {
+            term = i;
+        }
+
+        cout << term;
+
+        if (i < 100)
+        {
+            cout << ",";
+        }
+    }
+    cout<<endl;
+}
