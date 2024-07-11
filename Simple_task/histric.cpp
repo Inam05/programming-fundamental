c460f787fd06206a8e96e0b7a90b7c52cd2c3238
Inam05 <inamulhassan.tech@gmail.com>
2024-07-11 12:28:40 +0500

    Hello World in histeric

---
+++ Simple_task/histric.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"*\t*\n*\t*\n*********\n*\t*\n*\t*\n\n"<<"*********\n*\n*********\n*\n*********\n\n"<<"*\n*\n*\n*\n*********\n\n"<<"*\n*\n*\n*\n*********\n\n"<<"*********\n*\t*\n*\t*\n*\t*\n*********";
+}
