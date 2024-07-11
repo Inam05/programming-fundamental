0a2c6734a351eb5dbe808c9ce5fa3609aa8f55a6
Inam05 <inamulhassan.tech@gmail.com>
2024-07-11 12:25:56 +0500

    Area of the circle

---
+++ Simple_task/AreaOfCircle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x=3.1415;
+    float r;
+    cout<<"Enter the radius of the Circle :";
+    cin>>r;
+    cout<<"Area of the Circle :";
+    cout<<x*r*r;
+}
