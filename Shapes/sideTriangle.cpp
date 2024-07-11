cf4119abf08edf28e56d3c6ec3f6d55bf594a762
Inam05 <inamulhassan.tech@gmail.com>
2024-07-11 08:27:48 +0500

    Draw side triangle in c++

---
+++ Shapes/sideTriangle.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the number of rows for the pyramid: ";
+    cin >> a;
+    int rows,spac,col;
+    for (rows = 1; rows<=a; rows++)
+    {
+        // loop for space
+        for (spac=1;spac<=a-rows;spac++)
+        {
+            cout<<" ";
+        } 
+        //loops for star printing
+        for (col=1;col<=rows;col++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
